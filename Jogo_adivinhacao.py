bd9205527b63d0a3534caaf983499985034457dd
Geovane-DEV20 <geovaneDEV1998@gmail.comm>
2021-04-03 17:45:09 -0300

    Nova versão

--- Jogo_adivinhacao.py
+++ Jogo_adivinhacao.py
@@ -1,19 +1,12 @@
 import random
-<<<<<<< HEAD
-=======
 import time
->>>>>>> Initial commit
 print('*********************************')
 print('Bem vindo ao jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = random.randrange(1,101) # Essa função gera número aleatório. Round = Arredonda pro mais próximo.
-<<<<<<< HEAD
 total_tentativas = 3
-=======
-total_tentativas = 0
 pontos = 100
-
 
 print('Qual nível de dificuldade? ', numero_secreto)
 print('(1) Fácil (2) Médio (3) Difícil')
@@ -26,7 +19,6 @@ elif nivel == 2:
     total_tentativas = 10
 else:
     total_tentativas = 5
->>>>>>> Initial commit
 
 for rodada in range(1, total_tentativas + 1):
     print(f'Tentativas {rodada} de {total_tentativas}')
@@ -43,25 +35,18 @@ for rodada in range(1, total_tentativas + 1):
     menor = chute < numero_secreto
 
     if acertou:
-<<<<<<< HEAD
         print('Você acertou')
-=======
         print(f'Você acertou e fez {pontos} pontos')
->>>>>>> Initial commit
         break #Não vai continuar rodando
     else:
         if(maior):
             print('Você errou. O seu chute foi maior que o número secreto!')
         elif(menor):
             print('Você errou. O seu chute foi menor que o número secreto!')
-<<<<<<< HEAD
-    print('Fim do Jogo')
-print(numero_secreto)
-=======
         pontos_perdidos = abs(pontos-(numero_secreto - chute)) #40-60. Incluido a função abs= número absoluto
         pontos = pontos - pontos_perdidos
 
 print('Fim do Jogo')
 print(f'O número correto era: {numero_secreto}')
 time.sleep(5.5) # Pause 5.5 seconds
->>>>>>> Initial commit
+
